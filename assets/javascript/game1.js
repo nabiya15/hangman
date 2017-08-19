b6afd1d5c891b09adce3fea32d597302dda2f06f
m.nabiya@gmail.com <m.nabiya@gmail.com>
2017-08-19 08:21:54 -0500

    finally working fine

--- assets/javascript/game1.js
+++ assets/javascript/game1.js
@@ -1,20 +1,29 @@
-var inventions=['wheel','television','telephone','paper','printing press','light bulb','steam engine','lens','ink', 'internet','bicycle','gunpowder','battery','steam turbine','microscope','transistor','cathode ray tube','penicillin',];
-var winSound = new Audio("assets/sounds/win.mp3");
-var looseSound= new Audio("assets/sounds/looser.mp3");
-var win=0;
-var guessCount=0;
+  var inventions=['wheel','television','telephone','paper','printing press','light bulb','steam engine','lens','ink', 'internet','bicycle','gunpowder','battery','steam turbine','microscope','transistor','cathode ray tube','penicillin',];
+  var winSound = new Audio("assets/sounds/win.mp3");
+  var looseSound= new Audio("assets/sounds/looser.mp3");
+  var win=0;
+  var guessCount;
+  var maxGuess;
+  var guessList; //displays all the keys user
+  var answer;
+  var match;
+  var chosenWord;
+  var isWinner;
+  var isLooser;
 
-function init(){
-  var maxGuess=6;
-  
-  var guessList=[]; //displays all the keys user
-  var answer=[];
-  var remainingLetters;
-  
-  var match=false;
-  document.getElementById("status").innerHTML="Start Guessing...."
+  function reset (){
+   guessCount=0;
+   maxGuess=6;
+   guesslist=[];
+   
+   document.getElementById("guessedWords").innerHTML.guesslist;
+   answer=[];
+   isWinner=false;
+   isLooser=false;
+
+   
   // select a random word to be guessed
-  var chosenWord= inventions[Math.floor(Math.random()*inventions.length)]; //selects the word to be guessed
+  chosenWord= inventions[Math.floor(Math.random()*inventions.length)]; //selects the word to be guessed
   console.log(chosenWord); 
   answer=chosenWord.split(); 
 
@@ -26,62 +35,64 @@ function init(){
       answer[i]= " _";
     }
   }
-  document.getElementById("word").innerHTML=answer.join("");
-  document.getElementById("guesses").innerHTML=maxGuess;
-
-  document.onkeyup=function function_name(keypress) {
  
-    var userGuess=[keypress.key];
-
-    if((keypress.keyCode>=65 && keypress.keyCode<=90)|| keypress.keyCode==32)
-    {
-      guessList.push(" "+userGuess);
-      document.getElementById("guessedWords").innerHTML=guessList;
-    }
-
-    for (var j = 0; j < chosenWord.length; j++) {
-  //replace mask with correct guesses
- 
-       if(chosenWord.charAt(j)==userGuess){
-        answer[j]=userGuess;
-        match=true;
-      }
-    }
-
-    if(match!=true){
-    maxGuess--;
-   }
-
-    document.getElementById("word").innerHTML=answer.join(" ");
-    document.getElementById("guesses").innerHTML=(maxGuess);
- 
- 
-        remainingLetters=answer.length;
-        for (var i = 0; i < answer.length; i++) {
-          if(answer[i] !== " _"){
-            remainingLetters-=1;
-          }
-        }
-
-        if(remainingLetters===0) {
-
-          showThisMessage="You Won";
-          win+=1;
-          winSound.play();
-          document.getElementById("wins").innerHTML=win;
-          document.getElementById("status").innerHTML="Here's another one!"
-
-          init();
-        }
-
-        if(maxGuess===0){
-          looseSound.play();
-          alert("Sorry!! You ran out of guesses");
-
-          document.getElementById("status").innerHTML="Here's another one!"
-          init();  
-        }
-      
-    }
 }
-window.onload=init;
+
+reset();
+
+function updateGame(){
+   document.getElementById("status").innerHTML="Start Guessing...."
+  document.getElementById("word").innerHTML=answer.join(" ");
+  document.getElementById("guesses").innerHTML=maxGuess;
+}
+
+updateGame();
+
+document.onkeyup=function (keypress) {
+
+  var userGuess=keypress.key;
+
+  if((keypress.keyCode>=65 && keypress.keyCode<=90)|| keypress.keyCode==32)
+  {
+    guessList.push(" "+userGuess);
+    document.getElementById("guessedWords").innerHTML=guessList;
+  }else{
+    document.getElementById("status").innerHTML="Please use alphabets only!!!"
+    return;
+  }
+  match=false;
+  for (var j = 0; j < chosenWord.length; j++) {
+  //replace mask with correct guesses
+  if(chosenWord[j] === userGuess){
+    match=true;
+    answer[j]=userGuess;
+
+  }
+}
+
+if(!match){
+  maxGuess--;
+}
+
+updateGame();
+
+
+isWinner=answer.indexOf(" _")===-1;
+isLooser= maxGuess<=0;
+if(isWinner){
+  win++;
+  document.getElementById("status").innerHTML="Yay!! You Win."
+  document.getElementById("wins").innerHTML=win;
+  winSound.play();
+  reset();
+  updateGame();
+}
+if(isLooser){
+  document.getElementById("status").innerHTML="You Lost :( "
+  looseSound.play();
+  reset();
+  updateGame();
+}
+
+
+}
